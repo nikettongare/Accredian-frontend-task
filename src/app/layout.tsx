2c4390900747ed787760f519b73b0b03cd9897ce
nikettongare <nikettongaren24@gmail.com>
2023-12-07 17:38:00 +0530

    initial commit

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -15,6 +15,12 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
+      <head>
+        <meta
+          http-equiv="Content-Security-Policy"
+          content="upgrade-insecure-requests"
+        />
+      </head>
       <body className={``}>{children}</body>
     </html>
   )
